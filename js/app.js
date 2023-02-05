ffcff253a0cc71abfd224f511222f2e62b0eb924
Harrison Hdez <hhdezprogrammer@gmail.com>
2023-02-04 18:57:53 -0600

    weather-app

--- js/app.js
+++ js/app.js
@@ -9,5 +9,28 @@ window.addEventListener('load', () => {
 function buscarClima(e) {
     e.preventDefault();
 
-    console.log('Buscando el Clima...');
+    // console.log('Buscando el Clima...');
+
+    //* Validar
+    const ciudad = document.querySelector('#ciudad').value;
+    const pais = document.querySelector('#pais').value;
+
+    // console.log(ciudad);
+    // console.log(pais);
+
+    if (ciudad === '' || pais === '') {
+        mostrarError('Ambos campos son obligatorios');
+
+        return
+    }
+    
+}
+
+function mostrarError(mensaje) {
+    console.log(mensaje);
+
+    //* Crear una alerta
+    const alerta = document.createElement('div');
+
+    alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center');
 }
