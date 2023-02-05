5a1cd87581922893a73fc37d4b5111b993f2a29d
Harrison Hdez <hhdezprogrammer@gmail.com>
2023-02-04 19:06:46 -0600

    weather-app

--- js/app.js
+++ js/app.js
@@ -27,10 +27,27 @@ function buscarClima(e) {
 }
 
 function mostrarError(mensaje) {
-    console.log(mensaje);
+    // console.log(mensaje);
 
-    //* Crear una alerta
-    const alerta = document.createElement('div');
+    const alerta = document.querySelector('.bg-red-100');
+    
+    if(!alerta) {
+            //* Crear una alerta
+        const alerta = document.createElement('div');
 
-    alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center');
+        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center');
+
+        alerta.innerHTML = `
+            <strong class="font-bold">Error!</strong>
+
+            <span class="block">${mensaje}</span>
+        `;
+        container.appendChild(alerta);
+
+        //* Eliminar alerta 
+
+        setTimeout(() => {
+            alerta.remove();
+        }, 2000);
+    }
 }
