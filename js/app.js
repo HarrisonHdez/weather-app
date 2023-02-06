f5b1bb70f300cb93eb4c2abf444764f3a725c515
Harrison Hdez <hhdezprogrammer@gmail.com>
2023-02-05 22:38:16 -0600

    weather-app

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const container = document.querySelector('.container');
 const resultado = document.querySelector('#resultado');
 const formulario = document.querySelector('#formulario');
-
+   
 window.addEventListener('load', () => {
     formulario.addEventListener('submit', buscarClima);
 });
